c47fc23d85f6271fdbbc4f798a101efa8f027340
vp1927 <phamvietcuong2704@gmail.com>
2020-04-05 02:08:34 +0700

    add basic list

---
+++ src/TodoComplete.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './ToDoListItem.css';
+import Form from 'react-bootstrap/Form'
+
+class ToDoComplete extends Component {
+	
+}
+
+export default ToDoComplete;
---
+++ src/TodoList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './ToDoListItem.css';
+import Form from 'react-bootstrap/Form'
+
+class ToDoList extends Component {
+	
+}
+
+export default ToDoList;
