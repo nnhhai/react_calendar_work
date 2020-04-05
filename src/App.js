6b9c30d93b9ba95390d86f0f85229171e3e38cef
HanhNguyen98 <hanh.shin.1998@gmail.com>
2020-04-05 15:05:50 +0700

    add modal of noti check in next day

--- src/App.js
+++ src/App.js
@@ -1,20 +1,90 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import ChangeStateModal from "./ChangeStateModal";
+import "./Modal.css";
+var arrListYes = [];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toDoList: [
+        {
+          title: "ITSS",
+          description: "code React",
+          date: "2020-04-04",
+          state: -1,
+        },
+        {
+          title: "ITSS",
+          description: "code React",
+          date: "2020-04-06",
+          state: -1,
+        },
+      ],
+      isModal: false,
+      isCalendar: true,
+    };
 
-function App() {
-  return (
-    <div className="App">
-      <div className="Calendar">
-        <FullCalendar
-          defaultView="dayGridMonth"
-          plugins={[dayGridPlugin]}
+    // this.handlerChangedState = this.handlerChangedState.bind(this);
+  }
+
+  componentDidMount() {
+    var date = new Date();
+    date.setDate(date.getDate() - 1);
+    console.log(date.toISOString().split("T")[0])
+    this.state.toDoList.map((item, i) => {
+      if (item.date === date.toISOString().split("T")[0] && item.state === -1)
+        arrListYes.push(item);
+    });
+    if (arrListYes.length > 0)
+      this.setState({ isCalendar: false, isModal: true });
+  }
+
+  handlerChangedState = (array) => {
+    console.log("===", array);
+    let arr3 = this.state.toDoList.map((item, i) =>
+      Object.assign({}, item, array[i])
+    );
+    this.setState({ isCalendar: true, toDoList: arr3, isModal: false });
+  };
+  CloseModal() {
+    this.setState({
+      modal: {
+        isOpen: false,
+      },
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.isCalendar ? (
+          <div className="Calendar">
+            <FullCalendar
+              plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+              events={this.state.toDoList}
+            />
+          </div>
+        ) : null}
+        <ChangeStateModal
+          content={{
+            className: "change-state-modal",
+            title: "Check your work yesterday",
+            message: "",
+          }}
+          toDoList={this.state.toDoList}
+          modalIsOpen={this.state.isModal}
+          requestClose={() => {
+            this.CloseModal();
+          }}
+          handlerChangedState={this.handlerChangedState}
         />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
